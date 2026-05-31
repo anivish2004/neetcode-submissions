23f76d5252cd4943ba4e91f5bc372ad8934501e0
anivish2004 <animeshviswakarma@gmail.com>
2026-05-31 13:27:59 +0530

    Add: combination-target-sum-ii - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+   public:
+    vector<vector<int>> ans;
+    vector<int> temp;
+
+    void solve(int start, int target, vector<int>& candidates) {
+        if (target == 0) {
+            ans.push_back(temp);
+            return;
+        }
+
+        for (int i = start; i < candidates.size(); i++) {
+            if (i > start && candidates[i] == candidates[i - 1]) continue;
+            if (candidates[i] > target) break;
+
+            temp.push_back(candidates[i]);
+            solve(i + 1, target - candidates[i], candidates);
+            temp.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        solve(0, target, candidates);
+        return ans;
+    }
+};
